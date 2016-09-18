749795f3e651d254c2d71c7a146ae1eeea6df6f2
lixiaoxiong123 <297878118@qq.com>
2016-09-18 20:08:34 +0800

    1
    1

--- src/com/atguigu/java/HelloWorld.java
+++ src/com/atguigu/java/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("my first git");
 		System.out.println("my first git01");
+		System.out.println("my first git02");
 	}
 }
